8755908bcf710218093e26fc4c5900f5e4ed0b67
Erik Egers <erikegers+github.com@gmail.com>
2025-05-19 21:32:44 +0200

    Implemented picker with MVC

--- image-displayer/image-displayer/LandingView.swift
+++ image-displayer/image-displayer/LandingView.swift
@@ -6,21 +6,40 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct LandingView: View {
+    
+    @State private var myimage: UIImage?
+    @State private var photosPickerItem: PhotosPickerItem?
+    
     var body: some View {
         VStack {
-            Button(action: {
-                        
-                    }) {
-                        Text("Pick Imaage")
-                            .padding()
-                            .background(.blue)
-                            .foregroundColor(.white)
-                            .cornerRadius(10)
-                    }
+            Image(uiImage: myimage ?? UIImage(named: "placeholderImage") ?? UIImage())
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 400, alignment: .topLeading)
+                .foregroundStyle(.tint)
+            PhotosPicker(selection: $photosPickerItem, matching: .images) {
+                Label("Select Image", systemImage: "photo")
+                                    .padding()
+                                    .foregroundColor(.white)
+                                    .background(Color.blue)
+                                    .cornerRadius(8)
+            }
         }
         .padding()
+        .onChange(of: photosPickerItem) { _, _ in
+            Task {
+                if let photosPickerItem,
+                   let data = try? await photosPickerItem.loadTransferable(type: Data.self) {
+                    if let image = UIImage(data: data) {
+                        myimage = image
+                    }
+                }
+                photosPickerItem = nil
+            }
+        }
     }
 }
 
